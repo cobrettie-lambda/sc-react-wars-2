8e9d3261cf79e3c68aa0c67869052813fa773837
Cobrettie <cobrettiegarner3@gmail.com>
2019-08-30 19:18:56 -0400

    install semantic-ui-css, import into app.js

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import People from './components/People/People';
+import 'semantic-ui-css/semantic.min.css';
 import './App.css';
 
 const App = () => {
