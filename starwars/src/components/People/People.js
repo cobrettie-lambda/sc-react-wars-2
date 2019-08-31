3974ac7e2cb267a4ebde14a145bf42df55d72a6b
Cobrettie <cobrettiegarner3@gmail.com>
2019-08-30 21:04:56 -0400

    begin styling using reactstrap

--- starwars/src/components/People/People.js
+++ starwars/src/components/People/People.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import '../StarWars.css';
+import { Card } from 'reactstrap';
 
 function People(props) {
     console.log(props);
     return (
         <div className="container">
-            <div className="">
-                <h2 className="">                {props.person} 
+            <Card>
+                <h2 className="person-header">
+                    {props.person}
                 </h2>
-                <p>Born {props.birthyear}</p>
-                <p>{props.height}cm</p>
-                <p>{props.mass}kg</p>
-            </div>
+                <p className="person-birth">
+                    Born: {props.birthyear}
+                </p>
+                <p className="person-height">
+                    Height: {props.height}cm
+                </p>
+                <p className="person-mass">
+                    Weight: {props.mass}kg
+                </p>
+            </Card>
         </div>
     );
 }
